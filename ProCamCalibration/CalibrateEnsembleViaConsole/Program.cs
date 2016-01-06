0b508520bcfc47d28294bbb6f3a6d252d7af001c
Anton Alenov <aa16212@ic.ac.uk>
2016-01-06 18:07:45 +0000

    Add new console-based project for launching TK operations

---
+++ ProCamCalibration/CalibrateEnsembleViaConsole/Program.cs
@@ -0,0 +1,135 @@
+﻿using RoomAliveToolkit;
+using System;
+using System.IO;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using System.Runtime.Serialization;
+using System.ServiceModel.Discovery;
+using System.Collections.ObjectModel;
+
+namespace CalibrateEnsembleViaConsole
+{
+    class Program
+    {
+        private static Collection<EndpointDiscoveryMetadata> DiscoverCameras()
+        {
+            var discoveryClient = new DiscoveryClient(new UdpDiscoveryEndpoint());
+            var findCriteria = new FindCriteria(typeof(KinectServer2));
+            findCriteria.Duration = new TimeSpan(0, 0, 2);
+            var services = discoveryClient.Find(findCriteria);
+            discoveryClient.Close();
+            Console.WriteLine("Found {0} Kinect servers.", services.Endpoints.Count);
+            return services.Endpoints;
+        }
+
+        private static Collection<EndpointDiscoveryMetadata> DiscoverProjectors()
+        {
+            var discoveryClient = new DiscoveryClient(new UdpDiscoveryEndpoint());
+            var findCriteria = new FindCriteria(typeof(ProjectorServer));
+            findCriteria.Duration = new TimeSpan(0, 0, 2);
+            var services = discoveryClient.Find(findCriteria);
+            discoveryClient.Close();
+            Console.WriteLine("Found {0} projector servers.", services.Endpoints.Count);
+            return services.Endpoints;
+        }
+
+        private static void DiscoverServers()
+        {
+            Console.WriteLine("Finding Kinect and projector servers...");
+            var findKServers =
+                Task<Collection<EndpointDiscoveryMetadata>>.Factory.StartNew(DiscoverCameras);
+            var findPServers =
+                Task<Collection<EndpointDiscoveryMetadata>>.Factory.StartNew(DiscoverProjectors);
+
+            var kServers = findKServers.Result;
+            var pServers = findPServers.Result;
+            ensemble = new ProjectorCameraEnsemble(pServers.Count, kServers.Count);
+            for (int i = 0; i < kServers.Count; ++i)
+            {
+                ensemble.cameras[i].hostNameOrAddress = kServers[i].Address.ToString();
+                ensemble.cameras[i].name = i.ToString();
+            }
+            for (int i = 0; i < pServers.Count; ++i)
+            {
+                ensemble.projectors[i].hostNameOrAddress = pServers[i].Address.ToString();
+                ensemble.projectors[i].name = i.ToString();
+            }
+
+            ensemble.Save(XMLfilename);
+            Console.WriteLine("Done.");
+        }
+
+        private static void SaveXML()
+        {
+            try
+            {
+                ensemble.Save(XMLfilename);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Could not save configuration file to disk.\n" + ex);
+                return;
+            }
+        }
+
+        private static void SaveToObj()
+        {
+            try
+            {
+                ensemble.SaveToOBJ(path, calibrationName);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Could not save file to disk.\n" + ex);
+                return;
+            }
+        }
+
+        public static ProjectorCameraEnsemble LoadXML()
+        {
+            var knownTypeList = new List<Type>();
+            knownTypeList.Add(typeof(Kinect2Calibration));
+            var serializer = new DataContractSerializer(typeof(ProjectorCameraEnsemble), knownTypeList);
+            var fileStream = new FileStream(XMLfilename, FileMode.Open);
+            var room = (ProjectorCameraEnsemble)serializer.ReadObject(fileStream);
+            fileStream.Close();
+            return room;
+        }
+
+        static string op;
+        static string path;
+        static string calibrationName;
+        static string XMLfilename;
+        static ProjectorCameraEnsemble ensemble;
+
+        static void Main(string[] args)
+        {
+            if (args.Length != 2)
+            {
+                Console.WriteLine("Incorrect number of arguments supplied!");
+            }
+            op = args[0];
+            path = args[1];
+            calibrationName = Path.GetFileName(path);
+            XMLfilename = Path.Combine(path, calibrationName + ".xml");
+
+            switch (op)
+            {
+                case "create":
+                    DiscoverServers();
+                    break;
+                case "acquire":
+                    Console.WriteLine("Acquiring...");
+                    Console.WriteLine("Acquire complete.");
+                    break;
+                case "solve":
+                    Console.WriteLine("Solving...");
+                    Console.WriteLine("Solved!");
+                    break;
+                default:
+                    Console.WriteLine("Unrecognised command {0}", op);
+                    break;
+            }
+        }
+    }
+}
